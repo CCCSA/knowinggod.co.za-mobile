17e03fd89a9c272c30cb85c1ed45211dbdb515d5
KATLEGO35 <katlegomolokomme35@gmail.com>
2019-10-19 05:54:47 +0200

    fix changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 // appbrowserView
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
+declare var externalJsFunction;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
